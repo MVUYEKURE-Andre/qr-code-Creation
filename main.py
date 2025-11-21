4936143020dafb27cd751bf756bfdec964edff8a
MVUYEKURE-Andre <151786795+MVUYEKURE-Andre@users.noreply.github.com>
2025-11-21 16:26:01 +0200

    Initial commit

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 import qrcode
+url=input("Enter the Url: ").strip() # catching users information to make qr cod
+file_path="C:\\Users\\user\\OneDrive\\Desktop\\qr_code.png" #where to be saved on laptop
+
+qr=qrcode.QRCode() #create qr code object
+
+qr.add_data(url) # add content to qr code you just created
+
+img=qr.make_image()   #generating qr code image
+img.save(file_path) # save our qr code on our computer location
